b8b70dc8eaa92c57d8a95e5d7c69a3a673aaeb05
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-19 09:45:11 -0300

    Webpack funcionando corretamente

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -18,7 +18,7 @@ renderApp(App)
 
 if (module.hot) {
   module.hot.accept('./app', () => {
-    const NextApp = require('./app').defaut
+    const NextApp = require('./app').default
     renderApp(NextApp)
   })
 }
